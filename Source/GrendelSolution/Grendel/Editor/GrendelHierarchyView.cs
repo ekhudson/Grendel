36e9114d565d0b1d01a10cd1745749f76e992e4e
ekhudson <ekhudson@gmail.com>
2015-02-09 08:25:04 -0800

    +Visual Studio solution added +Grendel.DLL added

---
+++ Source/GrendelSolution/Grendel/Editor/GrendelHierarchyView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Grendel.Editor
+{
+    [InitializeOnLoad]
+    public class GrendelHierarchyView
+    {
+        static GrendelHierarchyView()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
+        }
+
+        private static void OnHierarchyWindowItemOnGUI(int instanceID, Rect position)
+        {
+            GUI.Button(position, "Hello");
+        }
+    }
+}
