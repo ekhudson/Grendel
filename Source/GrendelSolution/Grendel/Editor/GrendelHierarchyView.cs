e213268e3ef0d4a702dd7ab85149b896cf2ed01f
ekhudson <ekhudson@gmail.com>
2015-02-09 20:24:47 -0800

    +Blocking out hierarchy view

--- Source/GrendelSolution/Grendel/Editor/GrendelHierarchyView.cs
+++ Source/GrendelSolution/Grendel/Editor/GrendelHierarchyView.cs
@@ -9,14 +9,131 @@ namespace Grendel.Editor
     [InitializeOnLoad]
     public class GrendelHierarchyView
     {
+        private static float sIconWidth = 16f;
+        private static float sIconBufferWidth = 2f;
+        private static float sIconRightMargin = 8f;
+        private static float sIndentWidth = 14f;
+
+        private static string sLockButtonStyleName = "IN LockButton";
+        private static GUIStyle sLockButtonStyle = null;
+
+        private static GUIStyle sHideButtonStyle = null;
+        private static GUIContent sHideButtonContent = null;
+        private static bool testHideShow = false;
+
+        private static GUIStyle sTreeViewStyle = null;
+        private static float sTreeViewOffset = 24f;
+
+        private static int sCurrentParentCount = 0;
+        private static int sCurrentChildCount = 0;
+        private static int sCurrentIndentAmount = 0;
+        private static int sPreviousIndentAmount = 0;
+
         static GrendelHierarchyView()
         {
             EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
         }
 
+        private static void SetupStyles()
+        {
+            sLockButtonStyle = sLockButtonStyleName;
+
+            sHideButtonStyle = new GUIStyle(EditorStyles.whiteLabel);
+            sHideButtonStyle.fontSize = (int)sIconWidth;
+
+            sTreeViewStyle = new GUIStyle(GUIStyle.none);
+            sTreeViewStyle.fontSize = 13;
+        }
+
         private static void OnHierarchyWindowItemOnGUI(int instanceID, Rect position)
         {
-            GUI.Button(position, "Hello");
+            sCurrentIndentAmount = (int)( (position.x / sIndentWidth) -  1);
+
+            if (sLockButtonStyle == null)
+            {
+                SetupStyles();
+            }
+
+            GameObject gameObject = (GameObject)EditorUtility.InstanceIDToObject(instanceID);
+
+            sCurrentChildCount = gameObject.GetComponentsInChildren<Transform>().Length - 1;
+            sCurrentParentCount = gameObject.GetComponentsInParent<Transform>().Length - 1;
+
+            Rect iconPosition = new Rect(position);
+            iconPosition.width = sIconWidth;
+            iconPosition.x = (position.width - sIconWidth) + sIconRightMargin;
+
+            DrawLockButton(gameObject, iconPosition);
+
+            iconPosition.x -= (sIconWidth + sIconBufferWidth);
+
+            DrawHideButton(gameObject, iconPosition);
+
+            iconPosition.x -= (sIconWidth + sIconBufferWidth);
+
+            DrawTreeBranch(gameObject, position);
+
+            DrawPreview(gameObject, position);
+
+            sPreviousIndentAmount = sCurrentIndentAmount;
+        }
+
+        private static void DrawLockButton(GameObject gameObject, Rect iconPosition)
+        {
+            iconPosition.x += (sIndentWidth * sCurrentIndentAmount);
+
+            GUI.Toggle(iconPosition, false, string.Empty, sLockButtonStyleName);
+        }
+
+        private static void DrawHideButton(GameObject gameObject, Rect iconPosition)
+        {
+            iconPosition.x += (sIndentWidth * sCurrentIndentAmount);
+
+            GUI.color = testHideShow ? Color.gray : Color.white;
+
+            sHideButtonContent = testHideShow ?
+                EditorGUIUtility.IconContent("ViewToolOrbit On") :
+                EditorGUIUtility.IconContent("ViewToolOrbit");
+
+            testHideShow = GUI.Toggle(iconPosition, testHideShow, sHideButtonContent, sHideButtonStyle);
+            
+            GUI.contentColor = Color.white;
+        }
+
+        private static void DrawTreeBranch(GameObject gameObject, Rect iconPosition)
+        {
+            iconPosition.x -= sTreeViewOffset;
+
+            if (sCurrentParentCount <= 0)
+            {
+                return;
+            }
+
+
+            GUI.Label(iconPosition, "├", sTreeViewStyle);
+
+            if (sCurrentParentCount <= 1)
+            {
+                return;
+            }
+
+            iconPosition.x -= (7 * sCurrentParentCount);
+
+            GUI.Label(iconPosition, "│", sTreeViewStyle);
+          
+        }
+
+        private static void DrawPreview(GameObject gameObject, Rect position)
+        {
+            position.x -= sIconWidth;
+            position.width = sIconWidth;
+
+            GUIContent preview = EditorGUIUtility.ObjectContent(gameObject, gameObject.GetType());
+
+            if (preview.image != null)
+            {
+                GUI.DrawTexture(position, preview.image);
+            }
         }
     }
 }
