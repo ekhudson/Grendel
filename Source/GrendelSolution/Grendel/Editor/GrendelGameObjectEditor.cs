1e4ca49309149cc8326458c87035bb52258721e3
Elliot - Steambox <ekhudson@gmail.com>
2015-02-17 23:44:26 -0800

    +Gameobject extension added

---
+++ Source/GrendelSolution/Grendel/Editor/GrendelGameObjectEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+using Grendel.Extensions;
+
+namespace Grendel.Editor
+{
+    [InitializeOnLoad]
+    public class GameObjectEditorInitializer
+    {
+        static GameObjectEditorInitializer()
+        {
+            if (EditorApplication.update != GameObjectEditor.Update)
+            {
+                EditorApplication.update += GameObjectEditor.Update;
+            }
+        }
+    }
+
+    public class GameObjectEditor : UnityEditor.Editor
+    {
+        private static HashSet<UnityEngine.Object> sPreviousSelectedGameObjects = new HashSet<UnityEngine.Object>();
+
+        public static void Update()
+        {
+            // Object[] objects = FilterObjects(Selection.objects);
+            Selection.objects = FilterObjects(Selection.objects);
+
+
+            //if (sPreviousSelectedGameObjects.Count > 0 && Selection.objects.Length > 0)
+            //{
+            //    if (!sPreviousSelectedGameObjects.SetEquals(objects))
+            //    {
+            //        Selection.objects = objects;
+            //        sPreviousSelectedGameObjects = new HashSet<Object>(objects);
+            //    }
+            //}
+            //else if (Selection.objects.Length > 0)
+            //{
+            //    Selection.objects = objects;
+            //    sPreviousSelectedGameObjects = new HashSet<Object>(objects);
+            //}
+        }
+
+        private static UnityEngine.Object[] FilterObjects(UnityEngine.Object[] objects)
+        {
+            List<UnityEngine.Object> newSelection = new List<UnityEngine.Object>(Selection.objects);
+
+            for (int i = newSelection.Count - 1; i >= 0; i--)
+            {
+                if (newSelection[i] == null)
+                {
+                    newSelection.RemoveAt(i);
+                    continue;
+                }
+
+                if ((newSelection[i] as GameObject).IsLocked()) //filter out locked gameobjects
+                {
+                    newSelection.RemoveAt(i);
+                }
+            }
+
+            return newSelection.ToArray();
+        }
+
+        public void OnSelectionChange()
+        {
+            Debug.Log("HEY");
+        }
+    }
+}
---
+++ Source/GrendelSolution/Grendel/Extensions/GrendelGameObjectExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Grendel.Extensions
+{
+    public static class GrendelGameObjectExtensions
+    {
+        public static bool IsLocked(this GameObject gameObject)
+        {
+            return (gameObject.hideFlags & HideFlags.NotEditable) != 0;
+        }
+
+        public static void SetLock(this GameObject gameObject, bool locked, bool recursive)
+        {
+            if (locked)
+            {
+                if (!gameObject.IsLocked())
+                {
+                    gameObject.hideFlags |= (HideFlags.NotEditable | HideFlags.HideInInspector);
+
+                    if (recursive && gameObject.transform.childCount > 0)
+                    {
+                        Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+
+                        foreach (Transform child in children)
+                        {
+                            child.gameObject.hideFlags |= HideFlags.NotEditable | HideFlags.HideInInspector;
+                        }
+                    }
+                }
+            }
+            else if (gameObject.IsLocked())
+            {
+                gameObject.hideFlags &= ~(HideFlags.NotEditable | HideFlags.HideInInspector);
+
+                if (recursive && gameObject.transform.childCount > 0)
+                {
+                    Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+
+                    foreach (Transform child in children)
+                    {
+                        child.gameObject.hideFlags &= ~HideFlags.NotEditable | ~HideFlags.HideInInspector;
+                    }
+                }
+            }
+        }
+    }
+}
